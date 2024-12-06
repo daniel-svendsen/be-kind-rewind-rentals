395143641f9f46767ae70a2cb61aceab6dc63165
Daniel Svendsén <55035917+daniel-svendsen@users.noreply.github.com>
2024-12-06 11:32:12 +0100

    Delete rental directory

--- rental/src/main/java/se/yrgo/data/RentalRepository.java
+++
@@ -1,7 +0,0 @@
-package se.yrgo.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.yrgo.domain.Rental;
-
-public interface RentalRepository extends JpaRepository<Rental, Long> {
-}
